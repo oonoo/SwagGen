0b97f4258a95a99edecd09100efc85aa103e440f
Onno Bergob <o.bergob@mytaxi.com>
2020-05-14 15:14:41 +0200

    Make it build again
    Before it was updating to an incompatible SwiftCLI version.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
+        .package(url: "https://github.com/jakeheis/SwiftCLI", Package.Dependency.Requirement.exact("5.2.0")),
         .package(url: "https://github.com/yonaskolb/Stencil.git", from: "0.9.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0"),
         .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "3.3.0"),
